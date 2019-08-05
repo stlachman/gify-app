13d8595aa5980e54bdb9dfb35470d0fc84861b25
Seth Lachman <lachmanseth17@gmail.com>
2019-08-05 17:33:51 -0400

    Updated search bar to use hooks.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,78 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: ""
-    };
-  }
+const SearchBar = props => {
+  const [query, updateQuery] = useState("");
 
-  handleChange = event => {
-    this.setState({
-      query: [event.target.value]
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.searchGifs(this.state.query);
-  };
-
-  render() {
-    const { handleChange, handleSubmit } = this;
-    return (
-      <form
-        onSubmit={handleSubmit}
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        props.searchGifs(query);
+      }}
+      css={css`
+        display: flex;
+      `}
+    >
+      <label
         css={css`
+          color: #fff;
           display: flex;
+          align-items: center;
         `}
+        htmlFor="gif-search"
       >
-        <label
+        Search for GIPHs
+        <input
+          onChange={e => updateQuery(e.target.value)}
+          type="text"
+          name="gif-search"
+          value={query}
+          id="gif-search"
+          placeholder="Search GIPHS"
           css={css`
-            color: #fff;
-            display: flex;
-            align-items: center;
-          `}
-          htmlFor="gif-search"
-        >
-          Search for GIPHs
-          <input
-            onChange={handleChange}
-            type="text"
-            name="gif-search"
-            id="gif-search"
-            placeholder="Search GIPHS"
-            css={css`
-              margin-left: 10px;
-              border: 0;
-              background: transparent;
-              color: #161615;
-              font-size: 16px;
-              font-weight: bold;
-              padding: 8px 10px 8px 5px;
-              background: #fff;
-
-              &::placeholder {
-                color: #161615;
-              }
-            `}
-          />
-        </label>
-        <button
-          css={css`
-            background: #e0e1dc;
+            margin-left: 10px;
+            border: 0;
+            background: transparent;
+            color: #161615;
             font-size: 16px;
             font-weight: bold;
+            padding: 8px 10px 8px 5px;
+            background: #fff;
+
+            &::placeholder {
+              color: #161615;
+            }
           `}
-        >
-          Search Gifs
-        </button>
-      </form>
-    );
-  }
-}
+        />
+      </label>
+      <button
+        css={css`
+          background: #e0e1dc;
+          font-size: 16px;
+          font-weight: bold;
+        `}
+      >
+        Search Gifs
+      </button>
+    </form>
+  );
+};
 
 export default SearchBar;
