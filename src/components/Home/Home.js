8138e69e0942fbd23b380bdf4cdece033db949b6
Seth Lachman <lachmanseth17@gmail.com>
2019-08-17 20:04:21 -0400

    Updated class component to functional component.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,115 +1,29 @@
 /** @jsx jsx */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { css, jsx } from "@emotion/core";
 
 import SearchBar from "../SearchBar/SearchBar";
 
-class Home extends React.Component {
-  state = {
-    liked: [],
-    isLoading: true,
-    gifs: []
-  };
+const Home = () => {
+  const [gifs, setGifs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  render() {
-    const { gifs, isLoading } = this.state;
-
-    if (isLoading) {
-      return <div>Loading Gifs</div>;
-    }
-
-    return (
-      <main
-        css={css`
-          background: #f1e7fe;
-        `}
-      >
-        <div
-          css={css`
-            background: #00467f;
-            padding: 20px 10px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-          `}
-        >
-          <h1
-            css={css`
-              color: #fff;
-            `}
-          >
-            Giphy Trends!
-          </h1>
-          <SearchBar searchGifs={this.searchGifs} />
-        </div>
-        <div
-          css={css`
-            padding: 0 15px;
-            max-width: 950px;
-            width: 100%;
-            margin: 20px auto 0;
-          `}
-        >
-          <div
-            css={css`
-              display: grid;
-              grid-template-columns: repeat(2, 1fr);
-              grid-gap: 10px;
-            `}
-          >
-            {gifs &&
-              gifs.map(gif => {
-                return (
-                  <div
-                    css={css`
-                      background: #fff;
-                      display: flex;
-                      justify-content: center;
-                      flex-direction: column;
-                      align-items: center;
-                      padding: 10px;
-                    `}
-                    key={gif.id}
-                  >
-                    <div>
-                      <img
-                        css={css`
-                          height: auto;
-                          max-width: 100%;
-                        `}
-                        src={gif.images.downsized.url}
-                        alt=""
-                      />
-                    </div>
-                    <div
-                      css={css`
-                        margin-top: 20px;
-                      `}
-                    >
-                      <button onClick={this.storeLiked}>Like</button>
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
-        </div>
-      </main>
-    );
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get(
         `https://api.giphy.com/v1/gifs/trending?api_key=${
           process.env.REACT_APP_GIF_API
         }&limit=25`
       )
-      .then(res => this.setState({ gifs: res.data.data, isLoading: false }))
-      .catch(err => this.setState({ isLoading: false }));
-  }
+      .then(res => {
+        setGifs(res.data.data);
+        setIsLoading(false);
+      })
+      .catch(err => setIsLoading(true));
+  }, [isLoading]);
 
-  searchGifs = query => {
+  const searchGifs = query => {
     console.log(query);
     axios
       .get(
@@ -117,18 +31,97 @@ class Home extends React.Component {
           process.env.REACT_APP_GIF_API
         }&limit=15`
       )
-      .then(res => this.setState({ gifs: res.data.data }))
+      .then(res => setGifs(res.data.data))
       .catch(err => console.log(err));
   };
 
-  storeLiked = event => {
+  const storeLiked = event => {
     event.preventDefault();
     const likedImage = event.target.parentNode.parentNode.querySelector("img")
       .src;
-    this.setState({
-      liked: [...this.state.liked, { image: likedImage }]
-    });
   };
-}
+
+  if (isLoading) {
+    return <div>Loading Gifs</div>;
+  }
+  console.log(gifs);
+  return (
+    <main
+      css={css`
+        background: #a3aebd;
+      `}
+    >
+      <div
+        css={css`
+          background: #00467f;
+          padding: 20px 10px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        `}
+      >
+        <h1
+          css={css`
+            color: #fff;
+          `}
+        >
+          Giphy Trends!
+        </h1>
+        <SearchBar searchGifs={searchGifs} />
+      </div>
+      <div
+        css={css`
+          padding: 0 15px;
+          max-width: 950px;
+          width: 100%;
+          margin: 20px auto 0;
+        `}
+      >
+        <div
+          css={css`
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-gap: 10px;
+          `}
+        >
+          {gifs &&
+            gifs.map(gif => {
+              return (
+                <div
+                  css={css`
+                    background: #fff;
+                    display: flex;
+                    justify-content: center;
+                    flex-direction: column;
+                    align-items: center;
+                    padding: 10px;
+                  `}
+                  key={gif.id}
+                >
+                  <div>
+                    <img
+                      css={css`
+                        height: auto;
+                        max-width: 100%;
+                      `}
+                      src={gif.images.downsized.url}
+                      alt=""
+                    />
+                  </div>
+                  <div
+                    css={css`
+                      margin-top: 20px;
+                    `}
+                  >
+                    <button>Like</button>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Home;
